03804d79a773d07e732c54800d8e07a98ec9c4af
Intel Computers <Intel Computers@DESKTOP-EV5JP40>
2024-05-07 22:22:29 +0300

    Sending message by bot

---
+++ Controllers/BotController.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+
+namespace Trainers.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BotController : ControllerBase
+{
+    private readonly TelegramBotClient _botClient;
+    private const string TelegramBotToken = "7084182840:AAED416hYE51RxlghOvfUjCEtYFH3j7f_7c";
+    private const string AdminEmail = "murodilovshahriyor761@gmail.com";
+    private const string EmailFrom = "leadersacademy187@gmail.com";
+    private const string EmailPassword = "btij wwta hxkj agto";
+    private const string AdminChatId = "5795093154";
+
+    public BotController()
+    {
+        _botClient = new TelegramBotClient(TelegramBotToken);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessage([FromBody] string message)
+    {
+        try
+        {
+            string userData = $"Message: {message}";
+
+            using (var client = new HttpClient())
+            {
+                string telegramAPI = $"https://api.telegram.org/bot{TelegramBotToken}/sendMessage";
+
+                var messageData = new
+                {
+                    chat_id = AdminChatId,
+                    text = userData
+                };
+
+                var response = await client.PostAsJsonAsync(telegramAPI, messageData);
+                if (response.IsSuccessStatusCode)
+                {
+                    await SendEmailAsync(AdminEmail, "User Message", message);
+                    return Ok(new { Message = "Message sent successfully." });
+                }
+                else
+                {
+                    var errorMessage = await response.Content.ReadAsStringAsync();
+                    return BadRequest(new { Error = errorMessage });
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { Error = $"An error occurred: {ex.Message}" });
+        }
+    }
+
+
+    private async Task SendEmailAsync(string toEmail, string subject, string body)
+    {
+        using (var client = new SmtpClient("smtp.gmail.com", 587))
+        {
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential(EmailFrom, EmailPassword);
+
+            var mailMessage = new MailMessage
+            {
+                From = new MailAddress(EmailFrom),
+                Subject = subject,
+                Body = body
+            };
+            mailMessage.To.Add(toEmail);
+
+            await client.SendMailAsync(mailMessage);
+        }
+    }
+}
